88f214ed3f0e5d3b07e0253e8630daa01861140e
Nastya Izotina <nastya.izotina@mail.ru>
2023-03-28 21:48:20 +0300

    add some splash screen

---
+++ app/src/main/java/net/energogroup/akafist/StartActivity.java
@@ -0,0 +1,33 @@
+package net.energogroup.akafist;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if(Build.VERSION.SDK_INT <= Build.VERSION_CODES.R) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+            setContentView(R.layout.activity_start);
+
+            new Handler().postDelayed(() -> {
+                Intent i = new Intent(StartActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }, 2000);
+        }else {
+            Intent i = new Intent(StartActivity.this, MainActivity.class);
+            startActivity(i);
+            finish();
+        }
+    }
+}
